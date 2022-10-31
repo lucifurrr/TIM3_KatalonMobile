8c1d1a63e84a4df4e5b187f828ed6b35cfb835ab
Sandri Alfarisi <sandrialfarisi@gmail.com>
2022-10-31 11:34:35 +0700

    [Sandri] update script for coordinat

--- Scripts/Pages/homepage/Tap and Swipe Banner/Script1667113675831.groovy
+++ Scripts/Pages/homepage/Tap and Swipe Banner/Script1667113675831.groovy
@@ -1,21 +1,7 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+
+import com.kms.katalon.core.logging.KeywordLogger
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
 Mobile.waitForElementPresent(findTestObject('Homepage/imageview_Banner'), 5)
 
@@ -28,16 +14,26 @@ device_Height = Mobile.getDeviceHeight()
 device_Width = Mobile.getDeviceWidth()
 
 'Storing the startX,endX values by dividing device height by 2 Because Y coordinates are constant'
-int startY = device_Height / 2
+int startY = device_Height * 0.2
 
 'Here endY and startY values are equal for vertical Swiping for that assigning startY value to endY'
 int endY = startY
 
 'Storing the startX value'
-int startX = device_Width * 0.30
+int startX = device_Width / 1.2
 
 'Storing the endX value'
-int endX = device_Width * 0.70
+int endX = device_Width / 6
+
+//int startX = 900
+//int startY = 450
+//int endX = 180
+//int endY = 450
+
+
+//KeywordLogger log = new KeywordLogger()
+//log.logInfo("Start X =" + startX.toString(), "Start Y =" + startY.toString(), "End X =" + endX.toString(), "End Y =" + endY.toString())
+
 
 'Here Y constant for Swipe Vertical Left to Right'
 Mobile.swipe(startX, startY, endX, endY)
