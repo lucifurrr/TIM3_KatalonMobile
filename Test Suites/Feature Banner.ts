3a49a774b87717e5c51f8d4e83948814f3fe3839
Sandri Alfarisi <sandrialfarisi@gmail.com>
2022-11-01 15:58:41 +0700

    [Sandri] add test suite and report pesanan saya and banner feature

---
+++ Test Suites/Feature Banner.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Banner</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5707ddc5-4892-4fde-a97d-e7c63c236ab4</testSuiteGuid>
+   <testCaseLink>
+      <guid>608903a7-e3af-458e-ae8d-43d077628526</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/stepDefinition/Feature Banner/BANNER001 - Tap and Swipe Image Banner</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6108d2f0-dcbf-4956-8ae1-1a64b7f373d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/stepDefinition/Feature Banner/BANNER002 - Login to Tap and Swipe Image Banner</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Feature Pesanan Saya.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Pesanan Saya</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2565c1d5-853b-459c-a85b-8826739bf427</testSuiteGuid>
+   <testCaseLink>
+      <guid>63846f7c-a202-431f-9da8-af86a7dc65bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/stepDefinition/Feature Pesanan Saya/PESANAN001 - Click Pesanan Saya</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
